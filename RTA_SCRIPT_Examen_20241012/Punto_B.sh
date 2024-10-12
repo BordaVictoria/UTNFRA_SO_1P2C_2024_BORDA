c3f1de57e3311a2979bf6e9e444c104eeb785d6d
Borda Victoria <bordavictoria@hotmail.com>
2024-10-12 19:28:05 +0000

    ADD : Punto D y montaje persisitente en Punto B

--- RTA_SCRIPT_Examen_20241012/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241012/Punto_B.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 sudo fdisk /dev/sdc << EOF
 d
+d
+d
+d
+d
+d
+d
+d
+d
+d
 g
 n
 1
@@ -44,3 +53,20 @@ n
 
 w
 EOF
+
+for i in {1..10}; do
+  sudo mkfs.ext4 /dev/sdc$i
+done
+
+sudo mkdir -p /Examenes-UTN/profesores/parte{1..10}
+
+for i in {1..10}; do
+        sudo mount /dev/sdc$i /Examenes-UTN/profesores/parte$i
+done
+
+for i in {1..10}; do
+	echo "/dev/sdb$i /Examenes-UTN/profesores/parte$i ext4 default 0 0 0" | sudo tee -a /etc/fstab 
+done
+
+sudo mount -a 
+
---
+++ RTA_SCRIPT_Examen_20241012/Punto_D.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mkdir -p /home/vagrant/Estructura_asimetrica/{correo,clientes}/cartas_{1..100} 
+sudo mkdir -p /home/vagrant/Estructura_asimetrica/correo/carteros_{1..10}
+
