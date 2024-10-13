76fcb97e0101d20e07560fee42ff87510bfd8aeb
borda Victoria <bordavictoria@hotmail.com>
2024-10-13 17:45:20 +0000

    ADD_: intento montaje persistente

--- RTA_SCRIPT_Examen_20241012/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241012/Punto_B.sh
@@ -53,5 +53,10 @@ sudo mkdir -p /Examenes-UTN/profesores/parte{1,2,3,5,6,7,8,9,10,11}
 for i in {1,2,3,5,6,7,8,9,10,11}; do
         sudo mount /dev/sdc$i /Examenes-UTN/profesores/parte$i
 done
+for i in {1,2,3,5,6,7,8,9,10,11}; do
+	
+	echo "/dev/sdc$i /Examenes-UTN/profesores/parte$i ext4 defaults 0 0" | sudo tee -a /etc/fstab
+done
 
+sudo mount -a
 
